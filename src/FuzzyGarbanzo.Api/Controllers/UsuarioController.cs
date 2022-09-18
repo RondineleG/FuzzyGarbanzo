208a62d92571690b054e8407e89f570377224d8e
Rondinele Guimaraes <rondineleg@outlook.com>
2022-09-18 11:13:04 -0300

    Autenticando API com JWT

---
+++ src/FuzzyGarbanzo.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using FuzzyGarbanzo.Api.Controllers.Base;
+using FuzzyGarbanzo.Api.Dtos;
+using FuzzyGarbanzo.Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuzzyGarbanzo.Api.Controllers
+{
+    public class UsuarioController : MainController
+    {
+        private readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult SalvarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError("Ocorreu um erro ao salvar usuario", ex);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespostaDto()
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Erro = "Ocorreu um erro ao salvar usuario, tente novamente!"
+                });
+            }
+        }
+
+    }
+}
